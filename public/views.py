e4d1e751533431abd09dac944f58f5a9c637733c
AmalMohan487 <113880640+AmalMohan487@users.noreply.github.com>
2026-05-17 16:50:14 +0530

    latest

--- public/views.py
+++ public/views.py
@@ -603,13 +603,9 @@ import traceback
 from attendance.utils import check_and_send_attendance_warnings
 
 
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+
 @login_required
 def generate_report_and_send_alerts(request):
-    try:
-        check_and_send_attendance_warnings()
-        return HttpResponse("Low attendance emails sent successfully!")
-    except Exception:
-        return HttpResponse(
-            "<pre>" + traceback.format_exc() + "</pre>"
-        )
-        
+    return HttpResponse("View is working")
