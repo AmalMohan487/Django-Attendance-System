2e399ca4d47759816aafbeb4fb8aff185c528929
AmalMohan487 <113880640+AmalMohan487@users.noreply.github.com>
2026-05-17 21:48:23 +0530

    Enable attendance email alerts

--- public/email_views.py
+++ public/email_views.py
@@ -1,13 +1,15 @@
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from attendance.utils import check_and_send_attendance_warnings
 import traceback
 
 
 @login_required
 def generate_report_and_send_alerts(request):
     try:
-        # Send low attendance emails to students
+        # Import inside the function so any import errors are caught
+        from attendance.utils import check_and_send_attendance_warnings
+
+        # Send low attendance emails
         check_and_send_attendance_warnings()
 
         return HttpResponse(
