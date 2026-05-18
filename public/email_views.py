5939341c876c503da0146678aed8403b412b5164
AmalMohan487 <113880640+AmalMohan487@users.noreply.github.com>
2026-05-18 17:41:50 +0530

    Use Brevo API

--- public/email_views.py
+++ public/email_views.py
@@ -2,15 +2,16 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 import traceback
 
-from attendance.utils import (
-    check_and_send_attendance_warnings,
-    check_and_send_teacher_attendance_warnings,
-)
-
 
 @login_required
 def generate_report_and_send_alerts(request):
     try:
+        # Import inside the function to avoid slow startup
+        from attendance.utils import (
+            check_and_send_attendance_warnings,
+            check_and_send_teacher_attendance_warnings,
+        )
+
         student_count = check_and_send_attendance_warnings()
         teacher_count = check_and_send_teacher_attendance_warnings()
 
